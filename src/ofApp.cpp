d473fe911e506c672cae2667eaa6017e86d07739
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2017-05-16 16:55:29 -0500

    Added initial files to develop branch.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,331 +1,115 @@
 #include "ofApp.h"
 
-// t-SNE is dimension-reducing algorithm; raw t-SNE plot groups things 'randomly', but doesn't tell what group it's a part of
-// visually distinct, but eventually want to delineate boundaries and assign clusters to groups
-// k-means helps to identify groups that look similar (houses xy embedding + cluster index)
-
-const string allowed_ext[] = {"jpg", "png", "gif", "jpeg"};
-
-void ofApp::scan_dir_imgs(ofDirectory dir)
-{
-    ofDirectory new_dir;
-    
-    // size is equal to # of files in directory
-    int size = dir.listDir();
-    
-    // for every file
-    for (int i = 0; i < size; i++)
-    {
-        if (dir.getFile(i).isDirectory())
-        {
-            new_dir = ofDirectory(dir.getFile(i).getAbsolutePath());
-            new_dir.listDir();
-            new_dir.sort();
-            scan_dir_imgs(new_dir);
-        }
-        else if (std::find(std::begin(allowed_ext), std::end(allowed_ext), dir.getFile(i).getExtension()) != std::end(allowed_ext))
-        {
-            imageFiles.push_back(dir.getFile(i));
-        }
-    }
-}
-
 void ofApp::setup()
 {
-    ofSetVerticalSync(true);
+    ofSetLogLevel(OF_LOG_VERBOSE);
     
-    string imageDir = "/Users/tespin/Documents/openFrameworks/apps/myApps/00_BatchFeatureEncoder/bin/data/image-set-a-scanner-darkly-2";
+    ofJson settings;
+    ofFile file("settings.json");
+    if (file.exists()) file >> settings;
     
-    if (imageDir == "")
+    ofJson _extensions = settings["file_extensions"];
+    
+    fileFilter = std::make_unique<ofxIO::RegexPathFilter>(settings.value("filename_filter", ".*jpg"));
+    
+    ofJson _layersToEncode = settings["layers_to_encode"];
+    
+    std::vector<size_t> layersToEncode;
+    
+    if(!_layersToEncode.empty())
+        for (auto& layer: _layersToEncode)
+            layersToEncode.push_back(layer);
+    else layersToEncode.push_back(6);
+    
+    bool encodeIntermediateLayers = settings.value("encode_intermediate_layers", true);
+    
+    std::string network = settings.value("network", "image-net-2012.sqlite3");
+    bool forceEncode = settings.value("force_encode", false);
+    std::size_t numWorkers = settings.value("num_workers", 7);
+    
+    for (std::size_t i = 0; i < numWorkers; ++i)
     {
-        ofLog(OF_LOG_ERROR, "You haven't specified a directory! Exiting ...");
-        ofExit();
-        return;
+        auto encoder = std::make_unique<CCVEncoder>(network, layersToEncode);
+        encoder->start();
+        encoders.push_back(std::move(encoder));
     }
     
-    // development
-    //  nx = 15;
-    //  ny = 10;
-    //  nz = 10;
+    // starts with these images
+    std::string images = settings["initial_directory_to_process"];
+    process(images);
     
-    // test
-    nx = 8;
-    ny = 8;
-    nz = 8;
+    ofSetBackgroundColor(0);
     
-    w = 256;
-    h = 256;
-    d = 256;
-    
-    perplexity = 50;
-    theta = 0.4;
-    
-    scale = 2;
-    
-    // get images from directory
-    ofLog() << "Gathering images...";
-    ofDirectory dir = ofDirectory(imageDir);
-    scan_dir_imgs(dir);
-    ofLog() << imageFiles.size() << " images";
-    
-    // check that there are enough images in directory
-    if (imageFiles.size() < NUMIMAGES)
-    {
-        ofLog(OF_LOG_ERROR, "There are less images in the directory than the grid size requested (nx*ny*nz="+ofToString((nx*ny*nz))+"). Exiting to save you trouble...");
-        ofExit(); // not enough images!
-        return;
-    }
-    
-    // load images
-    for (int i = 0; i < NUMIMAGES; i++)
-    {
-        if (i % 20 == 0) ofLog() << " - loading image " << i << " / " << nx * ny * nz << " (" << dir.size() << " in dir)";
-        images.push_back(ofImage());
-        images.back().load(imageFiles[i].getAbsolutePath());
-    }
-    
-    // resize images to w x h
-    for (int i = 0; i < images.size(); i++)
-    {
-        if (images[i].getWidth() > images[i].getHeight())
-        {
-            images[i].crop((images[i].getWidth()-images[i].getHeight()) * 0.5, 0, images[i].getHeight(), images[i].getHeight());
-        }
-        else if (images[i].getHeight() > images[i].getWidth())
-        {
-            images[i].crop(0, (images[i].getHeight()-images[i].getWidth()) * 0.5, images[i].getWidth(), images[i].getWidth());
-        }
-        images[i].resize(w, h);
-    }
-    
-    // setup ccv
-    ccv.setup("image-net-2012.sqlite3");
-    
-    // encode images with ccv
-    ofLog() << "Encoding images...";
-    for (int i = 0; i < images.size(); i++)
-    {
-        if (i % 20 == 0) ofLog() << " - encoding image " << i << " / " << images.size();
-        std::vector<float> encoding = ccv.encode(images[i], ccv.numLayers()-1);
-        encodings.push_back(encoding);
-    }
-    
-    // run t-SNE and loage image points to imagePoints
-    ofLog() << "Run t-SNE on images...";
-    tsneVecs = tsne.run(encodings, 3, perplexity, theta, true);
-    
-    // solve 3D assignment grid
-    for (auto t: tsneVecs) tsnePoints.push_back(ofVec3f(t[0], t[1], t[2]));
-    gridPoints = makeGrid(nx, ny, nz);
-    solvedGrid = solver.match(tsnePoints, gridPoints, false);
-    
-    // add raw t-SNE data to k-means clusterer
-    for (int i = 0; i < NUMIMAGES; i++)
-    {
-        instances[i].push_back(tsneVecs[i][0]);
-        instances[i].push_back(tsneVecs[i][1]);
-        instances[i].push_back(tsneVecs[i][2]);
-        clusterer.addSample(instances[i]);
-    }
-    
-    // set up clustering and train
-    clusterer.setNumClusters(NUMCLUSTERS);
-    clusterer.train();
-    clusters = clusterer.getClusters();
-    
-    // assign each instance to a cluster
-    for (int i = 0; i < clusters.size(); i++)
-    {
-        Element element;
-        element.assignClusterIndex(clusters[i]);
-        
-        elementVector.push_back(element);
-        
-    //    cout << "Element " << ofToString(i) << " " << ofToString(instances[i]) << " assigned to cluster " << ofToString(clusters[i]) << endl;
-    }
-    
-    // populate vector of meshes
-    for (int i = 0; i < NUMCLUSTERS; i++)
-    {
-        ofMesh mesh;
-        mesh.setMode(OF_PRIMITIVE_POINTS);
-        meshVector.push_back(mesh);
-        
-        colors[i] = ofColor(ofRandom(255), ofRandom(255), ofRandom(255)) ;
-    }
-    
-    // populate vector of verts
-    for (int i = 0; i < solvedGrid.size(); i++)
-    {
-        float x = scale * (nx - 1) * w * solvedGrid[i].x;
-        float y = scale * (ny - 1) * h * solvedGrid[i].y;
-        float z = scale * (nz - 1) * d * solvedGrid[i].z;
-        
-        ofVec3f pos(x, y, z);
-        posVector.push_back(pos);
-    }
-    
-    // populate meshes with verts
-    for (int i = 0; i < elementVector.size(); i++)
-    {
-        elementVector[i].setVertex(posVector[i]);
-        meshVector[elementVector[i].getClusterIndex()].addVertex(elementVector[i].getVertex());
-
-        // check cluster
-        // std::cout << "Instance " << ofToString(i) << "assigned to actual cluster " << ofToString(elementVector[i].getClusterIndex()) << std::endl;
-        
-        // check vertices
-        // std::cout << "Cluster:" << elementVector[i].getClusterIndex() << ", Instance: " << ofToString(i) << ", Vertex: " << ofToString(elementVector[i].getVertex()) << std::endl;
-    }
-    
-    // still not sure about these numbers?
-    initPos = ofPoint(1850, 1850, 2100);
-    gridSize = ofPoint(10000, 10000, 10000);
-    
-    marchingCubes.init(initPos, gridSize, 16, 16, 16);
-    
-    cam.setNearClip(0.1);
-    cam.setFarClip(50000);
-    
-    sphere.setRadius(50);
-    
-    //now it would be nice to write that data out to a file of some sort if possible
-    //for one reason: it takes a long time to test the code - this is worth 1-2 hours of work, because it will save 10 of testing
-    
-    //option 1: generate a 3d array of element Objects, so we have a 3d array of cluster IDs to convert into a voxel array
-    
-    // check mesh
-    // for (int i = 0; i < meshVector.size(); i++)
-    // {
-    //    std::cout << "Mesh: " << ofToString(i) << ", Verts: " << ofToString(meshVector[i].getVertices());
-    // }
-    
-    initGui();
-    setupGui();
 }
 
 void ofApp::update()
 {
-    // cycle through each gui
-    for (int i = 0; i < elementVector.size(); i++)
-    {
-        int specifiedCluster = elementVector[i].getClusterIndex();
-        
-        if (clustersGui[specifiedCluster].showCubes && !clustersGui[specifiedCluster].modelRendered)
-        {
-            marchingCubes.resetIsoValues();
-            auto vertices = meshVector[specifiedCluster].getVertices();
-            
-            for (int j = 0; j < meshVector[specifiedCluster].getNumVertices(); j++)
-            {
-                ofVec3f vertex = vertices.at(j);
-                ofPoint p = ofPoint(vertex.x, vertex.y, vertex.z);
-                marchingCubes.addMetaBall(p, 0.05);
-                
-                // std::cout << "Vertex: " << ofToString(vertex) << " added!" << std::endl;
-            }
-            
-            // std::cout << "Before update: " << ofToString(marchingCubes.getVertices()) << std::endl;
-            marchingCubes.update(0.0035, true);
-            // std::cout << "After update: " << ofToString(marchingCubes.getVertices()) << std::endl;
-            clustersGui[specifiedCluster].modelRendered = true;
-        }
-        
-        if (clustersGui[specifiedCluster].modelRendered && clustersGui[specifiedCluster].save)
-        {
-            clustersGui[specifiedCluster].save = false;
-            marchingCubes.saveModel(ofToDataPath("clusters_" + ofToString(specifiedCluster+1) + ".stl"));
-            marchingCubes.resetIsoValues();
-        }
-    }
-    // std::cout << "Marching cubes verts: " << ofToString(marchingCubes.getVertices()) << std::endl;
-    // std::cout << cam.getPosition() << std::endl;
+
 }
 
 void ofApp::draw()
 {
-    ofBackground(0);
-    cam.begin();
-    ofEnableDepthTest();
+    int x = 15;
+    int y = 15;
     
-    for (int i = 0; i < elementVector.size(); i++)
-    {
-        int specifiedCluster = elementVector[i].getClusterIndex();
-        
-        if (clustersGui[specifiedCluster].drawImages)
-        {
-            ofSetColor(255, 255, 255);
-            images[i].draw(posVector[i], images[i].getWidth(), images[i].getHeight());
-        }
-        
-        if (clustersGui[specifiedCluster].drawPointCloud)
-        {
-            ofSetColor(colors[clusters[i]]);
-            sphere.setPosition(posVector[i].x + (images[i].getWidth() / 2), posVector[i].y + (images[i].getHeight() / 2), posVector[i].z);
-            sphere.draw();
-        }
-        
-        if (clustersGui[specifiedCluster].drawMesh)
-        {
-            ofSetColor(colors[clusters[i]]);
-            meshVector[specifiedCluster].draw();
-        }
-        
-        if (clustersGui[specifiedCluster].showCubes)
-        {
-            ofSetColor(colors[clusters[i]]);
-            marchingCubes.drawFilled();
-        }
-    }
+    ofDrawBitmapString("Drop image files / folders \nto encode.", x, y);
     
-    ofDisableDepthTest();
-    cam.end();
-    drawGui();
-}
-
-void ofApp::initGui()
-{
-    // for every cluster, create a clustergui and push it back to vector of clusterguis
-    for (int i = 0; i < NUMCLUSTERS; i++)
+    x += 5;
+    y += 35;
+    
+    for (std::size_t i = 0; i < encoders.size(); ++i)
     {
-        ClusterGui clusterGui;
-        clustersGui.push_back(clusterGui);
+        std::stringstream ss;
+        ss << "Worker " << i << " - Jobs Queued: " << encoders[i]->size();
+        ofDrawBitmapString(ss.str(), x, y);
+        y += 15;
     }
 }
 
-void ofApp::setupGui()
+void ofApp::process(const std::filesystem::path& _path)
 {
-    // set up every gui
-    for (int i = 0; i < NUMCLUSTERS; i++)
+    std::filesystem::path path = ofToDataPath(_path, true);
+    
+    if (std::filesystem::is_directory(path))
     {
-        clustersGui[i].gui.setup();
-        clustersGui[i].gui.setName("Cluster: " + ofToString(i+1));
+        std::vector<std::filesystem::path> files;
         
-        clustersGui[i].gui.setPosition(0, clustersGui[i].gui.getHeight() * (i*6));
-        clustersGui[i].gui.add(clustersGui[i].drawImages.set("Draw Image Cluster: " + ofToString(i+1), true));
-        clustersGui[i].gui.add(clustersGui[i].drawPointCloud.set("Draw Point Cloud Cluster: " + ofToString(i+1), true));
-        clustersGui[i].gui.add(clustersGui[i].drawMesh.set("Draw Mesh", false));
-        clustersGui[i].gui.add(clustersGui[i].showCubes.set("Show Cubes", false));
-        clustersGui[i].gui.add(clustersGui[i].save.set("Save", false));
+        ofxIO::DirectoryUtils::list(path, files, true, fileFilter.get());
+        
+        // submit encode
+        for (auto& f: files) encode(f);
+    }
+    else if (std::filesystem::is_regular_file(path))
+    {
+        if (fileFilter->accept(Poco::Path(path.string()))) encode(path);
+    }
+    else
+    {
+        ofLogError("ofApp::dragEvent") << "Skipping irregular file: " << path.string();
     }
 }
 
-void ofApp::drawGui()
+void ofApp::encode(const std::filesystem::path& path)
 {
-    // draw each gui
-    for (int i = 0; i < NUMCLUSTERS; i++) clustersGui[i].gui.draw();
+    currentWorkerIndex = (currentWorkerIndex + 1) % encoders.size();
+    
+    if (!encoders[currentWorkerIndex]->encode(path))
+    {
+        ofLogError("ofApp::encode") << "Unable to submite image to encoder " << currentWorkerIndex << " : " << path;
+    }
 }
 
 void ofApp::keyReleased(int key)
 {
-    // for selected gui, if 's' key is pressed, save parameter is true
-    for (int i = 0; i < elementVector.size(); i++)
+
+}
+
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
+    ofLogNotice("ofApp::dragEvent") << "Got dropped files.";
+    
+    for (auto& file: dragInfo.files)
     {
-        int specifiedCluster = elementVector[i].getClusterIndex();
-        
-        if (clustersGui[specifiedCluster].modelRendered)
-        {
-            if (key == 's') clustersGui[specifiedCluster].save = true;
-        }
+        process(file);
     }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,91 +2,165 @@
 
 #include "ofMain.h"
 #include "ofxCcv.h"
-#include "ofxTSNE.h"
-#include "ofxAssignment.h"
-#include "ofxLearn.h"
-#include "ofxGui.h"
-#include "ofxMarchingCubes.h"
 #include "ofxIO.h"
-#include "Element.h"
 #include "Poco/BinaryWriter.h"
 #include "Poco/BinaryReader.h"
 #include "Poco/DeflatingStream.h"
 
-//#define NUMIMAGES 1500
-//#define NUMCLUSTERS 11
-
-#define NUMIMAGES 512
-#define NUMCLUSTERS 5
-
-struct ClusterGui
+class CCVEncoder
 {
-    ofxPanel gui;
-    ofParameter<bool> drawImages;
-    ofParameter<bool> drawPointCloud;
-    ofParameter<bool> drawMesh;
-    ofParameter<bool> showCubes;
-    ofParameter<bool> save;
+public:
+    CCVEncoder(const std::string& network, std::vector<std::size_t> layersToEncode):
+        _network(network),
+        _isRunning(true),
+        _layersToEncode(layersToEncode)
+    {
+        
+    }
     
-    bool modelRendered;
+    ~CCVEncoder()
+    {
+        stop();
+        
+        if (_thread.joinable())
+        {
+            try
+            {
+                _thread.join();
+            }
+            catch (const std::exception& exc)
+            {
+                std::cout << exc.what() << std::endl;
+            }
+        }
+    }
+    
+    void start()
+    {
+        _thread = std::thread(&CCVEncoder::run, this);
+    }
+    
+    void stop()
+    {
+        _isRunning = false;
+        _channel.close();
+    }
+    
+    bool encode(const std::filesystem::path& file)
+    {
+        return _channel.send(file);
+    }
+    
+    std::size_t size() const
+    {
+        return _channel.size();
+    }
+    
+private:
+    void run()
+    {
+        _isRunning = true;
+        
+        while (_isRunning)
+        {
+            std::filesystem::path path;
+            
+            if (_channel.receive(path))
+            {
+                for (auto& layer: _layersToEncode)
+                {
+                    std::stringstream newExtension;
+                    newExtension << "." << _network << "." << layer << ".dat";
+                    
+                    std::filesystem::path file = std::filesystem::path(path);
+                    std::filesystem::path encodedFile = file;
+                    encodedFile += newExtension.str();
+                    
+                    if (!_forceEncode && std::filesystem::exists(encodedFile))
+                    {
+                        continue;
+                    }
+                    
+                    ofPixels pixels;
+                    
+                    // load image
+                    if (!ofLoadImage(pixels, path))
+                    {
+                        ofLogError("CCVEncoder::run") << "Unable to load >>" << path << "<<";
+                        continue;
+                    }
+                    
+                    if (!_ccv.isLoaded())
+                    {
+                        if (!_attemptedSetup)
+                        {
+                            _ccv.setup(_network);
+                            _attemptedSetup = true;
+                        }
+                        else
+                        {
+                            ofLogError("CCVEncoder::run") << "CCV could not be loaded with network: " << _network;
+                            break;
+                        }
+                    }
+                    
+                    std::vector<float> encoding = _ccv.encode(pixels, layer);
+                    
+                    //write encoded image to disk
+                    std::ofstream ostr(encodedFile.generic_string(), std::ios::binary);
+                    Poco::DeflatingOutputStream deflater(ostr, Poco::DeflatingStreamBuf::STREAM_GZIP);
+                    Poco::BinaryWriter writer(deflater);
+                    writer.writeBOM();
+                    writer << encoding.size();
+                    for (const auto& f: encoding) writer << f;
+                    writer.flush();
+                }
+            }
+        }
+        
+        _channel.close();
+    }
+    
+    // network path
+    std::string _network;
+    
+    // internal thread
+    std::thread _thread;
+    
+    // true if we attempted to load network
+    bool _attemptedSetup  =false;
+    
+    // truth if thread is running
+    std::atomic<bool> _isRunning;
+    
+    // queue of files to encode
+    ofxIO::ThreadChannel<std::filesystem::path> _channel;
+    
+    // true if encoder should force re-encode
+    bool _forceEncode = false;
+    
+    // libccv instance
+    ofxCcv _ccv;
+    
+    // layers to extract vectors for
+    std::vector<std::size_t> _layersToEncode;
 };
 
 class ofApp : public ofBaseApp{
 
 	public:
-        void setup();
-        void update();
-        void draw();
-        void initGui();
-        void setupGui();
-        void drawGui();
-        
-        void keyReleased(int key);
-    
-        void scan_dir_imgs(ofDirectory dir);
-    
-        std::vector<ClusterGui> clustersGui;
-        
-        ofxCcv ccv;
-        
-        ofxTSNE tsne;
-        std::vector<ofVec3f> tsnePoints;
-        
-        ofxLearnKMeans clusterer;
-        std::vector<double> instances[NUMIMAGES];
-        std::vector<int> clusters;
-        ofColor colors[NUMCLUSTERS];
-        
-        ofxMarchingCubes marchingCubes;
-        
-        ofPoint initPos, gridSize;
-        
-        ofEasyCam cam;
-    
-        ofxAssignment solver;
-        
-        std::vector<ofFile> imageFiles;
-        std::vector<ofImage> images;
-        std::vector<std::vector<float> > encodings;
-        std::vector<std::vector<ofxCcv::Classification> > results;
-        std::vector<std::vector<double> > tsneVecs;
-        std::vector<ofVec3f> solvedGrid, gridPoints;
-        std::vector<ofVec3f> initial, grid;
-        
-        std::vector<Element> elementVector;
-        
-        std::vector<ofVec3f> posVector;
-        
-        std::vector<ofMesh> meshVector;
-        
-        ofSpherePrimitive sphere;
-        
-        float xPos, yPos, zPos;
-        
-        int nx, ny, nz;
-        int w, h, d;
-        float perplexity, theta;
-        float scale;
-    
+		void setup();
+		void update();
+		void draw();
 
+		void keyReleased(int key);
+		void dragEvent(ofDragInfo dragInfo);
+    
+        void process(const std::filesystem::path& path);
+        void encode(const std::filesystem::path& path);
+        
+        std::vector<std::unique_ptr<CCVEncoder>> encoders;
+        std::size_t currentWorkerIndex = 0;
+        std::unique_ptr<ofxIO::RegexPathFilter> fileFilter;
+		
 };
